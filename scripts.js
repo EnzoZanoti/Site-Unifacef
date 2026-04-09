cc0fcbb61ee8385070816cede6a882415cdb3c49
EnzoZanoti <enzozanoti@gmail.com>
2026-04-09 11:07:19 -0300

    Calculadora atualizada para cada turma

--- scripts.js
+++ scripts.js
@@ -8,5 +8,17 @@
                     let qtdeMacarrao = Number(document.getElementById("qtdeMacarrao").value)
                     let pontos = (qtdeArroz * 10) + (qtdeFeijao * 8) + (qtdeOleo * 2) + (qtdeMacarrao * 4)
 
+                    let cor =Document.getElementById("cor").value
+                    let metaKit
+                    if (cor == "verde"){
+                        metaKit = 61
+                    }
+                    else if (cor == "vermelho"){
+                        metaKit = 32
+                    }
+                    else if (cor == "laranja"){
+                        metaKit = 21
+                    }
+
                     document.getElementById("result").innerText = "Pontuação: " + pontos
                 }
